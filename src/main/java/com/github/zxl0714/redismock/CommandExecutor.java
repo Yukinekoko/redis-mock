971ce2cdb1aa830a76c8d5ecfaffec2823d02b7a
snowmeow <yuki754685421@163.com>
2021-07-19 19:21:20 +0800

    feat: support subscribe publish
    [新增]1. 增加对select、publish指令的支持，暂缺少单元测试

--- src/main/java/com/github/zxl0714/redismock/CommandExecutor.java
+++ src/main/java/com/github/zxl0714/redismock/CommandExecutor.java
@@ -708,18 +708,24 @@ public class CommandExecutor {
      */
     public Slice publish(List<Slice> params) throws WrongNumberOfArgumentsException {
         checkArgumentsNumberEquals(params, 2);
-        return Response.integer(1);
+        int resp = base.publish(params.get(0), params.get(1));
+        return Response.integer(resp);
     }
 
     /**
      * 订阅
      */
     public Slice subscribe(List<Slice> params) throws WrongNumberOfArgumentsException {
-        checkArgumentsNumberEquals(params, 1);
+        checkArgumentsNumberGreater(params, 0);
+        SocketAttributes socketAttributes = SocketContextHolder.getSocketAttributes();
+        Preconditions.checkNotNull(socketAttributes);
         ImmutableList.Builder<Slice> builder = new ImmutableList.Builder<Slice>();
-        builder.add(Response.bulkString(new Slice("subscribe")));
-        builder.add(Response.bulkString(new Slice(params.get(0).toString())));
-        builder.add(Response.integer(1));
+        for (int i = 0; i < params.size(); i++) {
+            builder.add(Response.bulkString(new Slice("subscribe")));
+            builder.add(Response.bulkString(new Slice(params.get(i).toString())));
+            builder.add(Response.integer(i + 1));
+            base.subscribe(params.get(i), socketAttributes.getSocket());
+        }
         return Response.array(builder.build());
     }
 
--- src/main/java/com/github/zxl0714/redismock/RedisBase.java
+++ src/main/java/com/github/zxl0714/redismock/RedisBase.java
@@ -2,13 +2,16 @@ package com.github.zxl0714.redismock;
 
 import com.github.zxl0714.redismock.pattern.KeyPattern;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
 import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.*;
 
 /**
  * Created by Xiaolu on 2015/4/20.
@@ -18,6 +21,7 @@ public class RedisBase {
     private final Map<Slice, Slice> base      = Maps.newHashMap();
     private final Map<Slice, Long>  deadlines = Maps.newHashMap();
     private       List<RedisBase>   syncBases = Lists.newArrayList();
+    private static final Map<String, Set<Socket>> channels = Maps.newHashMap();
 
     public RedisBase() {
     }
@@ -128,5 +132,41 @@ public class RedisBase {
         for(RedisBase base : syncBases) {
             base.del(key);
         }
+    }
+
+    public synchronized void subscribe(Slice channelKey, Socket socket) {
+        String channelName = channelKey.toString();
+        Set<Socket> channel = channels.get(channelName);
+        if (channel == null) {
+            channel = Sets.newHashSet();
+            channels.put(channelName, channel);
+        }
+        channel.add(socket);
+    }
+
+    public synchronized int publish(Slice channelKey, Slice messageKey) {
+        String channelName = channelKey.toString();
+        Set<Socket> channel = channels.get(channelName);
+        if (channel == null) {
+            return 0;
+        }
+        Iterator<Socket> iterator = channel.iterator();
+        while (iterator.hasNext()) {
+            Socket socket = iterator.next();
+            try {
+                OutputStream outputStream = socket.getOutputStream();
+                ImmutableList.Builder<Slice> builder = new ImmutableList.Builder<Slice>();
+                builder.add(Response.bulkString(new Slice("message")));
+                builder.add(Response.bulkString(channelKey));
+                builder.add(Response.bulkString(messageKey));
+                Slice resp = Response.array(builder.build());
+                outputStream.write(resp.data());
+                outputStream.flush();
+            } catch (IOException e) {
+                // socket被关闭了就从订阅者Set中移除
+                iterator.remove();
+            }
+        }
+        return channel.size();
     }
 }
